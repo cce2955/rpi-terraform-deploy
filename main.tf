9be9161b4cf0c48f35754bc83115f760804b4a0d
cce2955 <cce29555@gmail.com>
2025-03-16 16:51:07 -0400

    working on database

--- main.tf
+++ main.tf
@@ -11,12 +11,12 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-# Nginx Image & Container
+# --- Nginx Web Server ---
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
 
-resource "docker_container" "nginx" {
+resource "docker_container" "nginx_server" {
   name  = "nginx-server"
   image = docker_image.nginx.name
 
@@ -28,14 +28,46 @@ resource "docker_container" "nginx" {
   restart = "always"
 }
 
-# Flask Backend
+# --- PostgreSQL Database ---
+resource "docker_image" "postgres" {
+  name = "postgres:latest"
+}
+
+resource "docker_volume" "postgres_data" {
+  name = "postgres_data"
+}
+
+resource "docker_container" "postgres_db" {
+  name  = "postgres-db"
+  image = docker_image.postgres.name
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+
+  volumes {
+    container_path = "/var/lib/postgresql/data"
+    volume_name    = docker_volume.postgres_data.name
+  }
+
+  env = [
+    "POSTGRES_USER=flaskuser",
+    "POSTGRES_PASSWORD=flaskpass",
+    "POSTGRES_DB=flaskdb"
+  ]
+
+  restart = "always"
+}
+
+# --- Flask Backend ---
 resource "docker_image" "flask_app" {
   name = "python:3.9-slim"
 }
 
 resource "docker_container" "flask_backend" {
   name  = "flask-backend"
-  image = "python:3.9-slim"
+  image = docker_image.flask_app.name
 
   ports {
     internal = 5000
@@ -44,19 +76,35 @@ resource "docker_container" "flask_backend" {
 
   restart = "always"
 
-command = [
-  "sh",
-  "-c",
-  <<-EOF
-    pip install flask &&
-    echo 'from flask import Flask
+  env = [
+    "DATABASE_URL=postgresql://flaskuser:flaskpass@postgres-db:5432/flaskdb"
+  ]
+
+  depends_on = [docker_container.postgres_db]
+
+  command = [
+    "sh", "-c",
+    <<-EOF
+    pip install flask psycopg2-binary &&
+    echo 'from flask import Flask, request, jsonify
+import psycopg2
 app = Flask(__name__)
-@app.route("/")
+conn = psycopg2.connect("dbname=flaskdb user=flaskuser password=flaskpass host=postgres-db")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS messages (id SERIAL PRIMARY KEY, content TEXT)")
+conn.commit()
+@app.route("/", methods=["GET"])
 def home():
-    return "Hello from Flask!"
+    return "Flask is connected to PostgreSQL!"
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.get_json()
+    cursor.execute("INSERT INTO messages (content) VALUES (%s) RETURNING id", (data["content"],))
+    conn.commit()
+    return jsonify({"message": "Inserted", "id": cursor.fetchone()[0]})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)' > app.py &&
     python3 app.py
-  EOF
-]
+    EOF
+  ]
 }
